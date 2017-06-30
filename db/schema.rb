5906767b37015cb0904ee528f0490780da46fc84
disne_y <bold.disney@gmail.com>
2017-06-30 18:03:36 +0300

    Create Answers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630140215) do
+ActiveRecord::Schema.define(version: 20170630145649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 20170630140215) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
