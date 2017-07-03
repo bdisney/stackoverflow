0c7eb73a758ea888c5b866bb867d1dc4b594ba82
disne_y <bold.disney@gmail.com>
2017-07-03 19:21:35 +0300

    Add acceptance spec for create question. Refactoring sign_in spec.

---
+++ spec/features/create_question_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'Create question', %q{
+  In ordere to get answer from community
+  As an authenticated user
+  I want to be able to ask question
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user creates question' do
+    sign_in(user)
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Test body'
+    click_on 'Create'
+
+    expect(page).to have_selector('#toastr-messages',
+                                  visible: false,
+                                  text: 'Question was created.')
+  end
+
+  scenario 'Non-authenticated user tries to create question' do
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to have_selector('#toastr-messages',
+                                  visible: false,
+                                  text: 'You need to sign in or sign up before continuing.')
+  end
+end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,27 +5,27 @@ feature 'User sign in', %q{
   As an user
   I want to be able to sign in
 } do
-  scenario 'Registered user trying to sign in' do
-    User.create!(email: 'test@test.com', password: '123456')
 
-    visit new_user_session_path
-    fill_in 'Email', with: 'test@test.com'
-    fill_in 'Password', with: '123456'
-    click_on 'Log in'
+  given(:user) { create(:user) }
+
+  scenario 'Registered user tries to sign in' do
+    sign_in(user)
 
     expect(page).to have_selector('#toastr-messages',
-                                  visible: false, text: 'Signed in successfully.')
+                                  visible: false,
+                                  text: 'Signed in successfully.')
     expect(current_path).to eq root_path
   end
 
-  scenario 'Unregistered user trying to sign in' do
+  scenario 'Unregistered user tries to sign in' do
     visit new_user_session_path
     fill_in 'Email', with: 'wrong-test@test.com'
     fill_in 'Password', with: '123456'
     click_on 'Log in'
 
     expect(page).to have_selector('#toastr-messages',
-                                  visible: false, text: 'Invalid Email or password.')
+                                  visible: false,
+                                  text: 'Invalid Email or password.')
     expect(current_path).to eq new_user_session_path
   end
 end
