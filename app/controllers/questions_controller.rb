89f444f9b3ee454c3581486229e5ed33edf45d69
disne_y <bold.disney@gmail.com>
2017-07-04 17:02:12 +0300

    Add 'User can view the question' spec. Fix question controller show method.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @answer = @question.answers.new
+    @answer = Answer.new
   end
 
   def new
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,6 +2,16 @@ FactoryGirl.define do
   factory :question do
     title 'MyTitle'
     body 'MyBody'
+
+    factory :question_with_answers do
+      transient do
+        answers_count 2
+      end
+
+      after(:create) do |question, evaluator|
+        create_list(:answer, evaluator.answers_count, question: question)
+      end
+    end
   end
 
   factory :invalid_question, class: 'Question' do
