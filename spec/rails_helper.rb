6f5dd050e4cc57d04733617f72b0963cfe49ff12
disne_y <bold.disney@gmail.com>
2017-07-19 15:12:11 +0300

    Configure capybara-webkit and database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,13 +33,39 @@ RSpec.configure do |config|
   config.include FeaturesHelper, type: :feature
   config.include Warden::Test::Helpers
 
+  Capybara.javascript_driver = :webkit
+
+  Capybara::Webkit.configure do |config|
+    config.block_unknown_urls
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
