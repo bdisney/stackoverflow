a69a798371b4b670894e38b0abdb459d5132d2f9
disne_y <bold.disney@gmail.com>
2017-07-25 18:07:03 +0300

    User can attach many files to answers and questions

--- spec/features/answers/add_files_to_answer_spec.rb
+++ spec/features/answers/add_files_to_answer_spec.rb
@@ -12,13 +12,29 @@ feature 'Add files to answer', %q{
   background do
     sign_in(user)
     visit question_path(question)
+    fill_in 'answer[body]', with: 'My awesome answer'
   end
 
   scenario 'Authenticated user add file when add answer', js: true do
-    fill_in 'answer[body]', with: 'My awesome answer'
-    attach_file 'File', "#{Rails.root}/spec/files/test_file_1.txt"
+    attach_file 'answer_attachments_attributes_0_file', "#{Rails.root}/spec/files/test_file_1.txt"
     click_on 'add answer'
 
     expect(page).to have_content('test_file_1.txt')
   end
+
+  scenario 'Authenticated user add many files when add answer', js: true do
+    first_file_field = first('input[type="file"]')
+    first_file_field.set "#{Rails.root}/spec/files/test_file_1.txt"
+
+    click_link('+ Add file')
+
+    second_file_field = all('input[type="file"]').last
+    second_file_field.set "#{Rails.root}/spec/files/test_file_2.txt"
+
+    click_on 'add answer'
+
+    expect(page).to have_content('test_file_1.txt')
+    expect(page).to have_content('test_file_2.txt')
+  end
 end
+
